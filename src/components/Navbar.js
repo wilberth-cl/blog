de9ac68c59b390025cc14c075732e391b63cad9b
wilberth-cl <wilberthfrancisco.cl@gmail.com>
2023-01-28 02:26:42 -0600

    Config: gh-pages-tres

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,27 @@
 
 export default function Navbar(){
+
+	const urlsn = {
+		algo: 'http://localhost:3000/algo',
+		otro: 'http://localhost:3000/otro',
+	}
+
+	const urls = {
+		algo: 'https://wilberth-cl.github.io/blog/algo',
+		otro: 'https://wilberth-cl.github.io/blog/otro',
+	}
+
+
 	return (
 
 			<nav className="nav">
 				<a href="/" className="site-title">Site Name</a>
 				<ul>
 					<li>
-						<a href="/algo">Algo</a>
+						<a href={urls.algo}>Algo</a>
 					</li>
 					<li>
-						<a href="/otro">Otro</a>
+						<a href={urls.otro}>Otro</a>
 					</li>
 				</ul>
 			</nav>
