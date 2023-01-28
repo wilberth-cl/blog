1dac541eb603a1617e725434566b7eed461cac99
wilberth-cl <wilberthfrancisco.cl@gmail.com>
2023-01-27 23:30:11 -0600

    Add: Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h5 className="text-white bg-sky-600 px-3"> Test Tailwind </h5>
-      </header>
-    </div>
+    <Navbar />  
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+
+export default function Navbar(){
+	return (
+
+			<nav className="nav">
+				<a href="/" className="site-title">Site Name</a>
+				<ul>
+					<li>
+						<a href="/Algo">Algo</a>
+					</li>
+					<li>
+						<a href="/Otro">Otro</a>
+					</li>
+				</ul>
+			</nav>
+
+
+		);
+}
